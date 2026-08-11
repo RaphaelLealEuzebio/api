1353017042f3005e6377eb2f86ee284eebca59f9
Raphael Leal Euzebio <raphaelndub@gmail.com>
2026-08-11 16:45:14 -0300

    feat: implement columns and relationship for UsuarioModel and EnderecoModel

--- models/usuario_model.py
+++ models/usuario_model.py
@@ -1,22 +1,27 @@
-from sqlalchemy import String,
-from sqlalchemy.orm import DeclarativeBase
-from sqlalchemy.orm import Mapped
+from sqlalchemy import ForeignKey, Integer, String
+from sqlalchemy.orm import Mapped, relationship
 from sqlalchemy.orm import mapped_column
 
 from core.database_core import Base
 
 class EnderecoModel(Base):
-    rua: Mapped[str] = mapped_column(String(30))
-    n:  Mapped[int] = mapped_column(Integer())
-    bairro:
-    estado:
-    pais:
+    __tablename__ = 'endereco_usuario'
+    
+    id :        Mapped[int] = mapped_column(primary_key=True)
+    rua:        Mapped[str] = mapped_column(String(30))
+    n:          Mapped[int] = mapped_column(Integer())
+    bairro:     Mapped[str] = mapped_column(String(50))
+    estado:     Mapped[str] = mapped_column(String(10))
+    pais:       Mapped[str] = mapped_column(String(10))
+
+    usuario_id: Mapped[int] = mapped_column(ForeignKey('usuarios.id'))
+    usuario:    Mapped['UsuarioModel'] = relationship(back_populates='endereco',uselist=False)
 
 class UsuarioModel(Base):
-    __tablename__ = 'user_account'
-    id: Mapped[int] = mapped_column(primary_key=True)
-    nome: Mapped[str] = mapped_column(String(30))
-    endereco: EnderecoModel
-    produto:
-    orcamento:
+    __tablename__ = 'usuarios'
     
+    id:       Mapped[int] = mapped_column(primary_key=True)
+    nome:     Mapped[str] = mapped_column(String(30))
+    senha_has:Mapped[str] = mapped_column(String(255))
+    email:    Mapped[str] = mapped_column(String(255), unique=True, index=True)
+    endereco: Mapped['EnderecoModel'] = relationship(back_populates='usuario',uselist=False)
