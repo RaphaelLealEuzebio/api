ddd60e38c7dd231426a58df260cc7ff7a170e328
Raphael Leal Euzebio <raphaelndub@gmail.com>
2026-08-11 16:45:43 -0300

    chore: fix column alignment in orcamento_schema

--- schemas/orcamento_schema.py
+++ schemas/orcamento_schema.py
@@ -9,23 +9,23 @@ class StatusOrcamento(str, Enum):
     expirado  = 'expirado'
 
 class OrcamentoRequest(BaseModel):
-    usuario_id: int = Field(gt=0)
-    valor: float = Field(gt=0)
+    usuario_id:        int = Field(gt=0)
+    valor:             float = Field(gt=0)
     valor_total_juros: float = Field(ge=0)
-    valor_parcela: float = Field(gt=0)
-    valor_entrada: float = Field(ge=0)
-    quantidade_parcela: int = Field(gt=0)
-    taxa_juros: float = Field(ge=0)
-    data_criacao: datetime
-    data_validade: datetime
-    status: StatusOrcamento
+    valor_parcela:     float = Field(gt=0)
+    valor_entrada:     float = Field(ge=0)
+    quantidade_parcela:int = Field(gt=0)
+    taxa_juros:        float = Field(ge=0)
+    data_criacao:      datetime
+    data_validade:     datetime
+    status:            StatusOrcamento
     
 
 class OrcamentoResponse(BaseModel):
-    usuario_id: int = Field(gt=0)
-    valor: float = Field(gt=0)
+    usuario_id:        int = Field(gt=0)
+    valor:             float = Field(gt=0)
     valor_total_juros: float = Field(ge=0)
-    valor_parcela: float = Field(gt=0)
-    valor_entrada: float = Field(ge=0)
-    status: StatusOrcamento
+    valor_parcela:     float = Field(gt=0)
+    valor_entrada:     float = Field(ge=0)
+    status:            StatusOrcamento
     
