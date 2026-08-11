3f3ae8e92535bc0038fbb8909dbe85375cd28f34
Raphael Leal Euzebio <raphaelndub@gmail.com>
2026-08-11 16:45:49 -0300

    feat: add data_criacao field to product schemas

--- schemas/produto_schema.py
+++ schemas/produto_schema.py
@@ -1,11 +1,17 @@
+from datetime import datetime
+
 from pydantic import BaseModel, Field
 
 
 class ProdutoRequest(BaseModel):
-    nome: str
-    valor: float = Field(gt=0)
-    descricao: str
+    nome:         str
+    valor:        float = Field(gt=0)
+    descricao:    str
+    data_criacao: datetime
+    
     
 class ProdutoResponse(BaseModel):
-    nome: str
-    valor: float
+    nome:         str
+    valor:        float
+    data_criacao: datetime
+    
