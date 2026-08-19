9f55713cdd501144d0e96bcee371fe90a9bf51b4
Raphael Leal Euzebio <raphaelndub@gmail.com>
2026-08-19 07:21:45 -0300

    feat: implement UsuarioRepository with user creation and error handling

---
+++ repositorys/usuario_repository.py
@@ -0,0 +1,30 @@
+from loguru import logger
+from sqlalchemy.exc import DatabaseError
+from sqlalchemy.orm import Session
+
+from models.usuario_model import EnderecoModel, UsuarioModel
+
+
+class UsuarioRepository:
+    def __init__(self, db: Session):
+        self.db = db
+
+    def criar(self, usuario:UsuarioModel) -> UsuarioModel | None:
+        try:
+            self.db.add(usuario)
+            self.db.commit()
+            self.db.refresh(usuario)
+
+            return usuario
+        except DatabaseError as error:
+            logger.error(f'Erro ao criar o usuario: {error}')
+
+    def deletar(self, usuario:UsuarioModel) -> UsuarioModel | None:
+        try:
+            self.db.delete(usuario)
+            self.db.commit()
+            self.db.refresh(usuario)
+
+            return usuario
+        except DatabaseError as error:
+           logger.error(f'Erro ao deleter o usuario{error}') 
